9be2134aae62fd40b5986287f165716398b5dae1
ayush nagar <nagarayush570@gmail.com>
2023-04-23 14:22:37 -0800

    first commit

---
+++ 42 exp module.js
@@ -0,0 +1,8 @@
+let name = 'ayush';
+let obj = {
+    name : 'ayush',
+    age : 18,
+    role : "front-end",
+    salary : 2000
+}
+export {name, obj}; // ek cheej ko ek baar hi export kar sakte hai 
